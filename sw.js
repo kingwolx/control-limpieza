a6b3057e56a74b84e467fc8d60cd1b6660e02d57
kingwolx <sebastianrubio19@gmail.com>
2026-08-19 21:19:11 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,16 +3,22 @@ const CACHE_NAME = 'control-limpieza-v1';
 const ARCHIVOS = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './sw.js'
 ];
 
 
-self.addEventListener('install', function(event) {
+/****************************************************
+ * INSTALAR
+ ****************************************************/
+
+self.addEventListener('install', event => {
 
   event.waitUntil(
 
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
+    caches
+      .open(CACHE_NAME)
+      .then(cache => {
 
         return cache.addAll(ARCHIVOS);
 
@@ -25,24 +31,23 @@ self.addEventListener('install', function(event) {
 });
 
 
-self.addEventListener('activate', function(event) {
+/****************************************************
+ * ACTIVAR
+ ****************************************************/
+
+self.addEventListener('activate', event => {
 
   event.waitUntil(
 
-    caches.keys()
-      .then(function(keys) {
+    caches
+      .keys()
+      .then(keys => {
 
         return Promise.all(
 
-          keys.map(function(key) {
-
-            if (key !== CACHE_NAME) {
-
-              return caches.delete(key);
-
-            }
-
-          })
+          keys
+            .filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
 
         );
 
@@ -55,15 +60,68 @@ self.addEventListener('activate', function(event) {
 });
 
 
-self.addEventListener('fetch', function(event) {
+/****************************************************
+ * PETICIONES
+ ****************************************************/
+
+self.addEventListener('fetch', event => {
+
+  const request =
+    event.request;
+
+
+  /*
+   * API
+   *
+   * NO intentamos cachearla.
+   * Si no hay Internet, el Index
+   * trabaja con la cola local.
+   */
+
+  if (
+    request.url.includes('/api')
+  ) {
+
+    return;
+
+  }
+
+
+  /*
+   * ARCHIVOS DE LA APLICACIÓN
+   *
+   * Primero intenta Internet.
+   * Si no hay Internet,
+   * utiliza la copia guardada.
+   */
 
   event.respondWith(
 
-    caches.match(event.request)
-      .then(function(response) {
+    fetch(request)
 
-        return response ||
-          fetch(event.request);
+      .then(response => {
+
+        const copia =
+          response.clone();
+
+        caches
+          .open(CACHE_NAME)
+          .then(cache => {
+
+            cache.put(
+              request,
+              copia
+            );
+
+          });
+
+        return response;
+
+      })
+
+      .catch(() => {
+
+        return caches.match(request);
 
       })
 
